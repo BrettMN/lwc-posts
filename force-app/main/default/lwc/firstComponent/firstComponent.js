4cf6551748139871a8a5051d0a43b8d0a4545bc0
Brett Nelson <brett.m.nelson@gmail.com>
2019-01-22 14:33:22 -0600

    pre props

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -1,9 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class FirstComponent extends LightningElement {
-  test(b, a) {
-    let c = [1, 2, 3, 4, 0, 5, 6, 7];
-
-    window.console.log(a, c, b);
-  }
-}
+export default class FirstComponent extends LightningElement {}
