0f9baaac34e976ddf567581e594b2cd7af96e7d4
Brett Nelson <brett.m.nelson@gmail.com>
2019-01-22 14:32:25 -0600

    reordering params

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class FirstComponent extends LightningElement {}
+export default class FirstComponent extends LightningElement {
+  test(b, a) {
+    let c = [1, 2, 3, 4, 0, 5, 6, 7];
+
+    window.console.log(a, c, b);
+  }
+}
