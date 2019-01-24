1f94a0db2445d649b39ace9260b38b51c9b3dc31
Brett Nelson <brett.m.nelson@gmail.com>
2019-01-23 23:18:22 -0600

    Public Properties

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class FirstComponent extends LightningElement {}
+export default class FirstComponent extends LightningElement {
+  @api label = 'WIPDeveloper.com  !!!';
+}
