c1d4b87acc4ec49bc975bfba256368111262fa88
Brett Nelson <brett.m.nelson@gmail.com>
2019-02-01 08:18:38 -0600

    post DOM events

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -2,4 +2,12 @@ import { LightningElement, api } from 'lwc';
 
 export default class FirstComponent extends LightningElement {
   @api label = 'WIPDeveloper.com  !!!';
+
+  handleClick() {
+
+    window.console.log('clicked');
+
+    this.label = this.label.split('').reverse().join('');
+
+  }
 }
