8f8f5b06c5f759ea874a9af870e27d89c7683a86
Brett Nelson <brett@WIPDeveloper.com>
2020-02-12 14:20:31 -0600

    loadScript issue

--- force-app/main/default/lwc/accessStaticResourceLoadScript/accessStaticResourceLoadScript.js
+++ force-app/main/default/lwc/accessStaticResourceLoadScript/accessStaticResourceLoadScript.js
@@ -10,35 +10,28 @@ export default class AccessStaticResourceLoadScript extends LightningElement {
     jsPath = jsPath;
     testFilesPath = testFiles;
 
-    builder;
-
     async build() {
-        const img = await this.loadImage(
-            `${this.testFilesPath}/WIPDeveloper.com-icon.png`
-        );
-
         const buildArea = this.template
             .querySelector('canvas.build-area')
             .getContext('2d');
-
-        loadScript(this, `${jsPath}/legra/lib/legra.umd.js`)
+        const img = await this.loadImage(
+            `${this.testFilesPath}/WIPDeveloper.com-icon.png`
+        );
+        loadScript(this, `${this.jsPath}/legra/lib/legra.umd.js`)
             .then(() => {
-                this.builder = new legra(buildArea, 5);
+                let builder = new legra(buildArea, 5);
+                // clear the build area before drawing again
+                buildArea.clearRect(0, 0, 600, 600);
+                builder.rectangle(12, 2, 8, 8);
+                builder.circle(15, 15, 12, { color: 'red' });
+                builder.ellipse(15, 15, 10, 6, {
+                    filled: true,
+                    color: 'green'
+                });
 
-                // buildArea.clearRect(0, 0, 600, 600);
-                // this.builder.rectangle(12, 2, 8, 8);
-
-                // this.builder.circle(15, 15, 12, { color: 'red' });
-                // this.builder.ellipse(15, 15, 10, 6, {
-                //     filled: true,
-                //     color: 'green'
-                // });
-
-                this.builder.drawImage(img, [0, 0]);
+                // builder.drawImage(img, [0, 0]);
             })
-            .catch(error => {
-                window.console.log({ error });
-            });
+            .catch(err => window.console.log({ err }));
     }
 
     async loadImage(src) {
